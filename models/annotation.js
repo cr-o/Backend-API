b7800b7230064a4f28ecbf5b545fe25335ce87a3
co <cherin.oh@outlook.com>
2024-09-01 22:01:35 -0400

    fleshing out model

--- models/annotation.js
+++ models/annotation.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
 
 const annotationSchema = new mongoose.Schema({
-  imageUrl: { type: String, required: true },
   label: { type: String, required: true },
   maskData: { type: String, required: true },
   area: { type: Number, required: true },
   annotator: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
+});
+
+function calculateArea() {
+
+}
+
+annotationSchema.pre('save', function(next) { // calculate area of maskData before saving to db
+  if (this.isModified('maskData')) {
+    this.area = calculateArea(this.maskData);
+  }
+  next();
 });
 
 module.exports = mongoose.model('Annotation', annotationSchema);
