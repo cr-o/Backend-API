5ba5588de95d5bff14feb8fc35eae47d8c3c25dc
co <cherin.oh@outlook.com>
2024-09-01 22:07:09 -0400

    starting controller

---
+++ controllers/annotationController.js
@@ -0,0 +1,13 @@
+const Annotation = require('../models/Annotation');
+
+exports.createAnnotation = async (req, res) => {
+
+};
+
+exports.searchAnnotations  = async (req, res) => {
+    
+};
+
+exports.rankAnnotations  = async (req, res) => {
+    
+};
