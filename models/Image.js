8e9bdf6a814c3cc90216866a02e2a9ef5c7f67db
co <cherin.oh@outlook.com>
2024-09-02 12:08:38 -0400

    adding image and project schemas for completeness

--- models/Image.js
+++ models/Image.js
@@ -1,6 +1,6 @@
 const imageSchema = new mongoose.Schema({
     filename: { type: String, required: true },
-    project_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Project', required: true },  // Reference to Project
+    projectId: { type: mongoose.Schema.Types.ObjectId, ref: 'Project', required: true },  // Reference to Project
     createdAt: { type: Date, default: Date.now }
   });
   
