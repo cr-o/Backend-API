03c6b56f55bbc48ada3141b48610b52000d8118a
co <cherin.oh@outlook.com>
2024-09-01 22:28:44 -0400

    createAnnotation

--- controllers/annotationController.js
+++ controllers/annotationController.js
@@ -1,13 +1,29 @@
 const Annotation = require('../models/Annotation');
+const calculateArea = require('../utils/calculateArea');
+
 
 exports.createAnnotation = async (req, res) => {
+    try {
+        const {label, maskData, annotator} = req.body;
+        const area = calculateArea(maskData)
+        const annotation = await Annotation.create({ label, maskData, area, annotator });
+        return res.status(201).json(annotation)
+    } catch (error) {
+        return res.status(400).json({ message: 'Error while creating annotation', error })
+}
 
-};
+exports.searchAnnotations = async (req, res) => {
+    try {
 
-exports.searchAnnotations  = async (req, res) => {
-    
-};
+    } catch (error) {
+        return res.status(404).json({ message: 'No annotations found' });
+    }
+}
 
-exports.rankAnnotations  = async (req, res) => {
-    
-};
+exports.rankAnnotations = async (req, res) => {
+    try {
+
+    } catch (error) {
+        return res.status(400).json({ message: 'Error while ranking annotations', error });
+    }
+}
