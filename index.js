0c8cf2e7bd5a62a5e29eb8fca142dde2f2493c42
co <cherin.oh@outlook.com>
2024-09-02 22:33:24 -0400

    formatting and ranking

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ app.get('/', (req, res) => {
 
 app.use('/api/annotations', require('./routes/annotations'))
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
